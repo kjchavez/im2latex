f88a13db5c2febd15b5f461941a6179d7b63c9e5
Kevin Chavez <kevin.j.chavez@gmail.com>
2016-11-06 16:31:50 -0800

    Added start to LSTM decoder, still missing one feedback loop.

--- model/decoder.py
+++ model/decoder.py
@@ -43,3 +43,58 @@ def attention(visual_features, h_t, hdim=256, vdim=512, adim=128, batch_size=2):
         att = tf.nn.softmax(logits)
         context = tf.reduce_sum(visual_features * tf.expand_dims(att, -1), 1)
         return att, context
+
+
+def token_prob(ht, context, prev_token, vocab_size, hparams={}):
+    hdim = hparams['hdim']
+    vdim = hparams['vdim']
+    odim = hparams['output_hidden_layer_dim']
+
+    with tf.variable_scope("output"):
+        L_h = variables.model_variable('L_h', shape=(hdim, odim),
+                                       initializer=tf.random_normal_initializer(
+                                            mean=0.0, stddev=np.sqrt(2.0/hdim)))
+        L_c = variables.model_variable('L_c', shape=(vdim, odim),
+                                       initializer=tf.random_normal_initializer(
+                                            mean=0.0, stddev=np.sqrt(2.0/vdim)))
+        L_o = variables.model_variable('L_o', shape=(odim, vocab_size),
+                                       initializer=tf.random_normal_initializer(
+                                            mean=0.0, stddev=np.sqrt(2.0/odim)))
+
+        # We might want a non-linearity between the matmuls.
+        logits = tf.matmul(tf.matmul(ht, L_h) + tf.matmul(context, L_c), L_o)
+        probs = tf.nn.softmax(logits, -1)
+        return logits, probs
+
+
+def decode(feat, lstm, state_tuple, attn_fn, vocab_size, hparams={}):
+    """ Runs a single step of decoding with attention.
+
+    Args:
+        feat: tensor of visual features
+        lstm: an LSTMCell to use for decoding
+        state_tuple: current state of 'lstm' cell, (c_state, m_state)
+        attn_fn: a function that returns 'soft-attention' weights and a context
+                 vector of same dims as a single visual feature vector
+
+    Returns:
+        (LSTM output, new state, attention weights)
+    """
+    hdim = hparams['hdim']
+    vdim = hparams['vdim']
+    adim = hparams['adim']
+    batch_size = hparams['batch_size']
+    c_state, m_state = state_tuple
+
+    attention, context = attn_fn(feat, m_state, hdim=hdim, vdim=vdim, adim=adim,
+                                 batch_size=batch_size)
+
+    # TODO(kjchavez):
+    # We probably want to loop in an embedded version of the output token from
+    # the previous step in the sequence, a la Show, Attend, and Tell.
+    x = context
+    output, state_tuple = lstm(x, state_tuple)
+    logits, probs = token_prob(state_tuple[0], context, None, vocab_size,
+                               hparams=hparams)
+    return probs, state_tuple, attention
+
--- test.py
+++ test.py
@@ -1,29 +1,46 @@
 import tensorflow as tf
 from model.input import get_train_data
 from model.encoder import convolutional_features
-from model.decoder import attention
+from model.decoder import attention, decode
 import numpy as np
 
-hdim = 256
-adim = 128
-batch_size = 2
+hparams = {
+    'hdim': 256,
+    'adim': 128,
+    'vdim': 512,
+    'batch_size': 2,
+    'output_hidden_layer_dim': 128
+}
 
-X, y = get_train_data(batch_size=batch_size)
+with open('character_mapping.txt') as fp:
+    char_mapping = [l for l in fp]
+    VOCAB_SIZE = len(char_mapping)
+
+X, y = get_train_data(batch_size=hparams['batch_size'])
 feat = convolutional_features(tf.expand_dims(X['image'], -1))
-h_0 = tf.zeros((batch_size, hdim))
-att, context = attention(feat, h_0, hdim=hdim, adim=adim, batch_size=batch_size)
+h_0 = tf.zeros((hparams['batch_size'], hparams['hdim']))
+c_0 = tf.zeros((hparams['batch_size'], hparams['hdim']))
+lstm = tf.nn.rnn_cell.BasicLSTMCell(hparams['hdim'])
+
+output, (c1, h1), att = decode(feat, lstm, (c_0, h_0), attention, VOCAB_SIZE, hparams)
+
+# att, context = attention(feat, h_0, hdim=hdim, adim=adim, batch_size=batch_size)
 init_op = tf.initialize_all_variables()
+
 
 with tf.Session() as sess:
     coord = tf.train.Coordinator()
     threads = tf.train.start_queue_runners(sess=sess, coord=coord)
     sess.run(init_op)
-    feat_val, att_val, c_val = sess.run([feat, att, context])
+    feat_val, att_val, out_val = sess.run([feat, att, output])
     coord.request_stop()
     coord.join(threads)
 
 print feat_val.shape
 print att_val.shape
-print att_val
-print np.sum(att_val[0])
-print c_val
+print out_val
+
+ML = np.argmax(out_val, 1)
+print ML
+print char_mapping[ML[0]]
+print char_mapping[ML[1]]
