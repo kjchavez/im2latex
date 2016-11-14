Fixed token loss weighting bug.

model/att_model.py
@@ -26,7 +26,10 @@ def model_fn(features, targets, mode, params):
     embeddings = embedding_matrix(VOCAB_SIZE, hparams['embedding_dim'])
     image = features['image']
     feat = convolutional_features(tf.expand_dims(image, -1))
-    target_token_seq = targets
+    target_token_seq = targets['target']
+    weights = targets['weights']
+    print "Weights:", weights.get_shape()
+    print "Targets:", target_token_seq.get_shape()
 
     lstm = LSTMSingleton.get_instance(hparams['hdim'])
 
@@ -38,12 +41,14 @@ def model_fn(features, targets, mode, params):
     prev_token = init_token
     logits_seq = []
     targets_seq = []
+    weights_seq = []
 
     # TODO(kjchavez): During 'inference' time, we actually want to decode until
     # we get a STOP token.
     with tf.variable_scope("decoder"):
         for i in xrange(hparams['unroll_length']):
             if (i > 0): tf.get_variable_scope().reuse_variables()
+            print "Iteration %d" % i
 
             logits, probs, (c, h), att = decode(feat, lstm, (c, h), attention,
                                                 prev_token, embeddings,
@@ -61,6 +66,7 @@ def model_fn(features, targets, mode, params):
 
                 # This is not actually *that* straightforward. Will need to revise.
                 targets_seq.append(target_token_seq[:, i])
+                weights_seq.append(weights[:, i])
 
                 prev_token = target_token_seq[:, i]
             else:
@@ -68,8 +74,8 @@ def model_fn(features, targets, mode, params):
 
     # Now add losses!
     if mode is tf.contrib.learn.ModeKeys.TRAIN:
-        weights = [tf.ones(hparams['batch_size'])]*hparams['unroll_length']
-        loss = tf.nn.seq2seq.sequence_loss(logits_seq, targets_seq, weights)
+        loss = tf.nn.seq2seq.sequence_loss(logits_seq, targets_seq,
+                                           weights_seq)
 
         global_step = variables.get_global_step()
         learning_rate = tf.train.exponential_decay(

model/input.py
@@ -7,6 +7,7 @@ train_filename = "/home/kevin/projects/im2latex/im2latex_train.tfrecord"
 dev_filename = "/home/kevin/projects/im2latex/im2latex_dev.tfrecord"
 
 PAD_ID = 0
+GO_ID = 1
 
 def preprocess(image):
     return image - 0.5
@@ -27,9 +28,6 @@ def get_feature_input(filepattern, batch_size=1):
         })
 
     label = tf.cast(features.pop('label'), tf.int32)
-    label = tf.sparse_to_dense(label.indices, label.shape, label.values,
-                       default_value=PAD_ID)
-    weights = tf.cast(tf.where(label != PAD_ID), tf.float32)
 
     image = tf.cast(tf.decode_raw(features['image_raw'], tf.uint8),
                      tf.float32)
@@ -45,7 +43,12 @@ def get_feature_input(filepattern, batch_size=1):
                                   dynamic_pad=True,
                                   num_threads=4)
 
-    return {'image': image}, label
+    label = tf.reshape(tf.sparse_to_dense(label.indices, label.shape, label.values,
+                               default_value=PAD_ID), (batch_size, -1))
+    weights = tf.reshape(tf.cast(tf.not_equal(label, PAD_ID), tf.float32),
+                         (batch_size, -1))
+
+    return {'image': image}, {'target': label, 'weights': weights}
 
 def get_train_data(batch_size=1):
     return get_feature_input(train_filename, batch_size=batch_size)

test.py
@@ -36,7 +36,11 @@ with tf.Session() as sess:
     coord = tf.train.Coordinator()
     threads = tf.train.start_queue_runners(sess=sess, coord=coord)
     sess.run(init_op)
-    print "Y:", y.eval()
+    y_val = sess.run(y)
+    print "Target:", y_val['target']
+    print "Weights:", y_val['weights']
+    print y_val['target'].shape
+    print y_val['weights'].shape
     feat_val, att_val, out_val = sess.run([feat, att, output])
     coord.request_stop()
     coord.join(threads)